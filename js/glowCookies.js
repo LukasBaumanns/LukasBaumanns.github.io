560fe7e680d07e476d477f092cc15c095bd7f8cc
LukasBaumanns <lukas.baumanns@gmail.com>
2026-09-04 08:24:52 +0200

    a

--- js/glowCookies.js
+++ js/glowCookies.js
@@ -31,19 +31,25 @@ class GlowCookies {
   addCss() {
     const stylesheet = document.createElement('link');
     stylesheet.setAttribute('rel', 'stylesheet');
-    stylesheet.setAttribute('href', 'css/glowCookies.css?v=2026-09-03');
+    stylesheet.setAttribute('href', 'css/glowCookies.css?v=2026-09-04');
     document.head.appendChild(stylesheet);
   }
 
   createDOMElements() {
     // COOKIES BUTTON
     this.PreBanner = document.createElement("div");
-    this.PreBanner.innerHTML = `<button type="button" id="prebannerBtn" class="prebanner prebanner__border__${this.config.bannerStyle} glowCookies__${this.config.position} glowCookies__${this.config.border} animation" style="color: ${this.banner.manageCookies.color}; background-color: ${this.banner.manageCookies.background};">
-                                    <svg fill="currentColor" style="margin-right: 7px; margin-top: 2px; vertical-align: text-top;" height="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    this.PreBanner.innerHTML = `<button type="button" id="prebannerBtn" class="prebanner prebanner__border__${this.config.bannerStyle} glowCookies__${this.config.position} glowCookies__${this.config.border} animation" aria-label="${this.banner.manageCookies.text}" title="${this.banner.manageCookies.text}" style="color: ${this.banner.manageCookies.color}; background-color: ${this.banner.manageCookies.background};">
+                                    <svg aria-hidden="true" fill="currentColor" height="18px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                         <path d="M510.52 255.82c-69.97-.85-126.47-57.69-126.47-127.86-70.17 0-127-56.49-127.86-126.45-27.26-4.14-55.13.3-79.72 12.82l-69.13 35.22a132.221 132.221 0 0 0-57.79 57.81l-35.1 68.88a132.645 132.645 0 0 0-12.82 80.95l12.08 76.27a132.521 132.521 0 0 0 37.16 72.96l54.77 54.76a132.036 132.036 0 0 0 72.71 37.06l76.71 12.15c27.51 4.36 55.7-.11 80.53-12.76l69.13-35.21a132.273 132.273 0 0 0 57.79-57.81l35.1-68.88c12.56-24.64 17.01-52.58 12.91-79.91zM176 368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"/>
-                                    </svg>${this.banner.manageCookies.text}</button>`;
+                                    </svg></button>`;
     this.PreBanner.style.display = "none";
-    document.body.appendChild(this.PreBanner);
+    const privacyButton = document.getElementById('datenschutz')
+    const privacyPanel = privacyButton && privacyButton.nextElementSibling
+    if (privacyPanel) {
+      privacyPanel.insertAdjacentElement('afterend', this.PreBanner)
+    } else {
+      document.body.appendChild(this.PreBanner)
+    }
 
     // COOKIES BANNER
     this.Cookies = document.createElement("div");
